0d8cd0aaabfc9414ac46085e464e7c60c2850046
Geibison Moura <geibison@hotmail.com>
2020-09-18 09:38:33 -0300

    Iniciando os projeto --- atualização vsCode,android Studio

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:filmesFavoritos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Home(),
+    title: "Filmes",
+  ));
+}
